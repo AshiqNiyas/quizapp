877c9b8cea94281022d9ec219880e05f23028d0d
Ashiq N <ashiqniyas.dev@gmail.com>
2023-02-17 10:34:14 +0530

    version 1.1

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ const optiondisplay = document.querySelector('.options')
 const questiondisplay = document.querySelector('.questiondis')
 const resultdisplay = document.querySelector('.result')
 const displayscore = document.getElementById('resultscore')
+const answerdiv = document.querySelector('.answerdiv')
+const realansw = document.getElementById('realansw')
 let score = 0
 let index = 0
 let questions;
 let farray;
 let myarray;
+let timeout;
+let correct;
 
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -66,24 +70,33 @@ async function getquiz(){
     myarray = shuffle(questions[index].incorrectAnswers)
     // optiondisplay.innerHTML = myarray
     myarray.forEach(opt => {
+      correct = questions[index].correctAnswer
       let btn = document.createElement('button')
       btn.classList.add('optionbtn')
       btn.innerText = opt
-      btn.addEventListener('click',()=>{
-        if(opt == questions[index].correctAnswer){
-            console.log('right');
-            score +=1
-        }
-        nextquestion()
-      })
       optiondisplay.append(btn)
+      btn.addEventListener('click',()=>{       
+        if (btn.innerText == correct){
+          score += 1
+          btn.classList.add('green')
+        }
+        else{
+          btn.classList.add('red')
+        }
+        realansw.innerHTML = `answer is ${correct}`
+        answerdiv.style.display = 'block'
+        setTimeout(nextquestion,2000)
+      })
       playerscore.innerHTML = score
+
+      
     });
   
     
   }
   
   function nextquestion(){
+    answerdiv.style.display = 'none'
     if(index == 4){
         questioncontainer.style.display = 'none'
         resultdisplay.style.display = 'flex'
